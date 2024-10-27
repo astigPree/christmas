6e0b33798d4758d17e13c414c7ec3255c6f474b5
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-10-27 13:30:45 +0800

    setup websocket

--- app/consumers.py
+++ app/consumers.py
@@ -4,13 +4,39 @@ from channels.generic.websocket import WebsocketConsumer
 class YourConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-        self.send(text_data=json.dumps({
-            'message': 'Hello! WebSocket connection established!'
-        }))
+        checking_message = {
+            'message': 'Magandang Pasko sa inyu!'
+            }
+        self.send(text_data=json.dumps(checking_message))
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        try:
+            
+            text_data_json = json.loads(text_data)
+            """
+                text_data_json = {
+                    'action': 'location',
+                    ...
+                }
+            """
+            
+            data_action = text_data.get('action' , None)
+            
+            if data_action == 'location':
+                lat = text_data_json.get('lat' , None)
+                long = text_data_json.get('long' , None)
+                if lat and long:
+                    # TODO: Check if the user is near on the tree
+                    pass
+            
+            
+            
+        except Exception as e:
+            # TODO: Log the error
+            self.send(text_data=json.dumps({'error': str(e)}))
+            print(e)
+        
+        
+
+    def disconnect(self, close_code):
+        pass
--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,68 @@
 
-var socket = new WebSocket('ws://' + window.location.host + '/ws/some_path/');
+let WebSocketHasError = false;
+let ResponseHasError = false;
 
-socket.onmessage = function(e) {
-    var data = JSON.parse(e.data);
-    console.log(data.message);
-};
 
-socket.onclose = function(e) {
-    console.error('WebSocket closed unexpectedly');
-};
+addEventListener('DOMContentLoaded', function() {
+    var socket = new WebSocket('ws://' + window.location.host + '/ws/check/location/');
 
-function sendMessage(message) {
-    socket.send(JSON.stringify({
-        'message': message
-    }));
-}
+    const sendWithApi = async () => {
+        // TODO:  USE API TO FETCH A DATA AS A SECOND OPTIONS
+        try{
+            let response = await fetch('/ws/check/location/');
+            
+            if (!response.ok){
+                let data = await response.json();
+                ResponseHasError = true;
+                return null;
+            }
+
+            return await response.json();
+
+        } catch (error) {
+            return null;
+        
+        }
+    }
+
+
+    socket.onmessage = function(e) {
+        // Use to receive a data
+        try{
+            var data = JSON.parse(e.data);
+        } catch (error) {
+            WebSocketHasError = true;
+            // TODO:  USE API TO FETCH A DATA AS A SECOND OPTIONS
+            let response = sendWithApi();
+            if (response){
+
+            }
+        }
+        
+        
+    };
+
+    socket.onclose = function(e) {
+        console.error('WebSocket closed unexpectedly');
+    };
+
+    function sendMessage(message) {
+        /* 
+            message = {
+                'action' : 'location',
+                ...
+            }
+        */
+       try {
+            socket.send(JSON.stringify(message));
+        } catch (error) {
+            WebSocketHasError = true;
+            // TODO:  USE API TO FETCH A DATA AS A SECOND OPTIONS
+            
+        }
+
+    }
+
+})
+
 
