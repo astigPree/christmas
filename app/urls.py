4271cb276794135a5176b45edf8000e54bfdccde
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-10-30 13:37:25 +0800

    collect static

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 
-
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('', view=views.list_of_trees, name='list_of_trees'),
+urlpatterns = [ 
+    path('', views.list_of_trees, name='list_of_trees'),
 ]
 
 
